e4fda165972256faa3af7a45c76cab1f7218550f
yashu <79122392+cloudshare360@users.noreply.github.com>
2025-06-15 17:39:17 +0000

    updated

--- install-angular-npm-start.sh
+++ install-angular-npm-start.sh
@@ -1,4 +1,5 @@
 # Install angular project node modules
 echo "📦 Installing npm modules for Angular Project..."
-cd /workspaces/angular-fullstack-dev/angular-creating-and-hosting-a-full-stack-site/SourceCode/ExerciseFiles/01_02/End/buy-and-sell && npm install 
+cd angular-creating-and-hosting-a-full-stack-site/SourceCode/ExerciseFiles/01_02/End/buy-and-sell && npm install 
 npm run start
+
